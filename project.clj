ea2f4b3372d01dee1191ebcc2cb7fac80c8f5d36
An Cao <kcj.pop.com@gmail.com>
2022-07-18 14:20:24 +0300

    Add hashp
    Signed-off-by: An Cao <kcj.pop.com@gmail.com>

--- project.clj
+++ project.clj
@@ -3,14 +3,16 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
-                 [ring "1.8.1"]
-                 [integrant "0.8.0"]
+  :dependencies [[clj-http "3.10.0"]
                  [environ "1.2.0"]
+                 [hashp "0.2.1"]
+                 [integrant "0.8.0"]
                  [metosin/reitit "0.5.5"]
+                 [ring "1.8.1"]
                  [seancorfield/next.jdbc "1.2.659"]
                  [sqlitejdbc "0.5.6"]
-                 [clj-http "3.10.0"]]
+                 [org.clojure/clojure "1.10.1"]]
+  :injections [(require 'hashp.core)]
   :main ^:skip-aot cheffy.server
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
